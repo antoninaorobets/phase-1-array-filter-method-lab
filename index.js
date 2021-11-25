e5c08e20b047a3fad28ce49fc12b55ea8b58e765
antoninaorobets <pani.antonina.don@gmail.com>
2021-11-25 13:32:36 -0500

    commit tree ab02edf0da66760624e8ac2d9eb0639a843bb8ba

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+
+
+function findMatching(drivers,muchName){
+    return drivers.filter((driver) => driver.toLowerCase() === muchName.toLowerCase() )
+
+}
+
+
+function f1(name,chars){
+return  (name.includs(chars) && name[0]=== chars[0])
+
+
+}
+
+function  fuzzyMatch(drivers,chars){
+    return drivers.filter((name) => name.startsWith(chars))
+
+
+}
+
+function matchName(drivers,string){
+    return drivers.filter((element) => element.name === string)
+
+}
